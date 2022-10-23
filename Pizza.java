819b4da8177d2fdc714c31e099e89454cb0a8e43
G.A <42187739+Kether180@users.noreply.github.com>
2022-10-23 23:14:08 +0200

    toppings

--- Pizza.java
+++ Pizza.java
@@ -1,17 +1,24 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Pizza extends Food {
 
-    private ArrayList<String> toppings = new ArrayList<String>();
-    private String name;
-    private double price;
+    private List<Food> toppings = new ArrayList<Food>();
+    // private String name;
+    // private double price;
 
     public Pizza(String name, double price) {
         super("pizza", 45);
 
     }
 
-    public ArrayList<String> addTopping(String topping) {
+    public List<Food> addTopping(String topping) {
+
+        Food pizza1 = new Food(null, 0); // subclass object
+        Food pizza2 = new Food(null, 0); // subclass object
+
+        toppings.add(pizza1); // toppings for the pizza
+        toppings.add(pizza2); // toppings for the pizza
 
         // price++;
 
@@ -23,3 +30,9 @@ public class Pizza extends Food {
     }
 
 }
+
+// Note: Parameters refers to the list of variables in a method declaration.
+// Arguments are the actual values that are passed in when the method is
+// invoked.
+// When you invoke a method, the arguments used must match the declaration's
+// parameters in type and order.
